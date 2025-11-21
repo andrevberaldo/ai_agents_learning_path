7825c14b106558162f75871fb328fd9ca547cce6
André V Beraldo <andreberaldo@hotmail.com>
2025-11-21 19:09:10 -0300

    working with tamplate

--- src/cache.py
+++ src/cache.py
@@ -1,45 +1,17 @@
 import os
 from dotenv import load_dotenv
 from langchain_openai import OpenAI
-from langchain_core.globals import set_llm_cache
-from langchain_community.cache import RedisCache, InMemoryCache
-from redis import Redis
+from src.llm_cache.cache import init_model_cache
 
-load_dotenv()
-
-def init_model_cache():
-    """
-        Function responsible to set llm cache.
-        If a redis host is provided through env variable, it will use redis based cache, otherwise will use in memory based cache.
-    """
-    redis_host = os.getenv("REDIS_HOST")
-
-    if not redis_host:
-        print("Using InMemoryCache")
-        
-        set_llm_cache(InMemoryCache())
-        return
-    
-    
-    print("Using RedisCache")
-    redis = Redis(
-        host=os.getenv("REDIS_HOST"),
-        port=os.getenv("REDIS_PORT")
-    )
-
-    set_llm_cache(RedisCache(
-        redis_= redis,
-        ttl=45
-    ))
-        
-    
 
 def main():
+    
     """
         Function responsible to instantiate a openai model to interact with.
     """
+    load_dotenv()
     init_model_cache()
-
+    
     openai = OpenAI(
         model=os.getenv("OPENAI_MODEL_COMPLETION"),
         max_tokens=100
