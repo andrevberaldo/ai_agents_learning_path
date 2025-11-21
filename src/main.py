7fafa1e60921ee2b008e425c1320ceabad6d1a36
André V Beraldo <andreberaldo@hotmail.com>
2025-11-21 08:09:54 -0300

    remove redis test

--- src/main.py
+++ src/main.py
@@ -1,21 +1,9 @@
 from dotenv import load_dotenv
-from redis import Redis
 import os
 
 load_dotenv()
 
-def main():
-    redis = Redis(
-        host=os.getenv("REDIS_HOST", "localhost"),
-        port=os.getenv("REDIS_PORT", 6379),
-        decode_responses=True
-    )
-
-    redis.set("key_1", "some value")
-
-    item = redis.get("key_1")
-
-    print(item)
+def main():    
 
 
 if __name__ == "__main__":
